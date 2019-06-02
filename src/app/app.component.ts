7641bf2ff2cecf25683d666d68bf12893d0316bf
Muata Nkosi <mnkosi@fandm.edu>
2019-06-02 06:13:46 -0400

    Add naive way of storing and getting public data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,17 +32,42 @@ export class AppComponent {
     ],
     hiddenDefaults: ['phone_number']
   }
-    constructor( private amplifyService: AmplifyService ) {
-        this.amplifyService.authStateChange$
-            .subscribe(authState => {
-                this.signedIn = authState.state === 'signedIn';
-                if (!authState.user) {
-                    this.user = null;
-                } else {
-                    console.log(this.user);
-                    this.user = authState.user;
-                    this.greeting = "Hello " + this.user.username;
-                }
-        });
-    }
+  constructor( private amplifyService: AmplifyService ) {
+    this.amplifyService.authStateChange$
+      .subscribe(authState => {
+        this.signedIn = authState.state === 'signedIn';
+        if (!authState.user) {
+          this.user = null;
+        } else {
+          console.log(this.user);
+          this.user = authState.user;
+          this.greeting = "Hello " + this.user.username;
+        }
+      });
+  }
+
+  store() {
+    console.log('button click');
+    this.amplifyService.storage().put('test.txt', 'Hello1')
+    .then (result => console.log(result)) // {key: "test.txt"}
+    .catch(err => console.log(err));
+  }
+
+  get() {
+    this.amplifyService.storage().get('test.txt')
+    .then(result => {
+      console.log(result)
+      var request = new XMLHttpRequest()
+
+      request.open('GET', result, true)
+      request.onload = function() {
+        // Begin accessing JSON data here
+        
+        console.log(this.response);
+        
+      }
+      request.send()
+    })
+    .catch(err => console.log(err));
+  }
 }
