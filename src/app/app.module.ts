56d5b484775160563ca49f9a31ffee09bce4bb6d
Muata Nkosi <mnkosi@fandm.edu>
2019-06-02 06:31:04 -0400

    Add routing and create login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AmplifyAngularModule, AmplifyService } from 'aws-amplify-angular';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit } from '@angular/core';
+import { AmplifyService } from 'aws-amplify-angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  signedIn: boolean;
+  user: any;
+  greeting: string;
+  signUpConfig = {
+    signUpFields: [
+      {
+      label:'Email Address',
+      key: 'email',
+      required: true,
+      displayOrder: 1,
+      type: 'string',
+      custom: false
+      },
+      {
+        label: 'Name',
+        key: 'name',
+        required: true,
+        displayOrder: 2,
+        type: 'string',
+        custom: false
+      }
+    ],
+    hiddenDefaults: ['phone_number']
+  }
+  constructor( private amplifyService: AmplifyService ) {}
+
+  ngOnInit() {
+    this.amplifyService.authStateChange$
+    .subscribe(authState => {
+      this.signedIn = authState.state === 'signedIn';
+      if (!authState.user) {
+        this.user = null;
+      } else {
+        console.log(this.user);
+        this.user = authState.user;
+        this.greeting = "Hello " + this.user.username;
+      }
+    });
+  }
+
+  store() {
+    console.log('button click');
+    this.amplifyService.storage().put('test.txt', 'Hello1')
+    .then (result => console.log(result)) // {key: "test.txt"}
+    .catch(err => console.log(err));
+  }
+
+  get() {
+    this.amplifyService.storage().get('test.txt')
+    .then(result => {
+      console.log(result)
+      var request = new XMLHttpRequest()
+
+      request.open('GET', result, true)
+      request.onload = function() {
+        // Begin accessing JSON data here
+        
+        console.log(this.response);
+        
+      }
+      request.send()
+    })
+    .catch(err => console.log(err));
+  }
+}
